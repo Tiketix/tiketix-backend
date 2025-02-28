feeeba3d789c097059e6c87ddabb4808fe4f6b43
Teesavage <teesavage077@gmail.com>
2025-02-28 15:34:55 +0100

    add bin and obj to gitignore

--- tiketix/Models/Entities/EventClient.cs
+++ tiketix/Models/Entities/EventClient.cs
@@ -3,18 +3,18 @@
 namespace tiketix.Models.Entities
 {
     public class EventClient
-{
-    public Guid Id { get; set; }
+    {
+        public Guid Id { get; set; }
 
-    public required string FirstName { get; set; }
-    
-    public required string LastName { get; set; }
+        public required string FirstName { get; set; }
+        
+        public required string LastName { get; set; }
 
-    public required string Email { get; set; }
-    
-    public string? Phone { get; set; }
+        public required string Email { get; set; }
+        
+        public string? Phone { get; set; }
 
-    public required string Password { get; set; }
+        public required string Password { get; set; }
 
-}
+    }
 }
